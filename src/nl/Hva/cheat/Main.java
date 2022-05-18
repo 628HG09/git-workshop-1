012237ed00cf0aff4eb83251a82b26d30e96cac5
628HG09 <s.rutges@gmail.com>
2022-05-18 15:37:45 +0200

    eerste pging

---
+++ src/nl/Hva/cheat/Main.java
@@ -0,0 +1,10 @@
+package nl.Hva.cheat;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Git les 1!");
+        System.out.println("eerste aanpassing");
+        System.out.println("tweede aanpassing");
+    }
+}
